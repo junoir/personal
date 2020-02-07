f9d2211e225c3f032006c28e1345b8d37ac7061e
Lucky Motseki <junoir.mklv@gmail.com>
2020-02-07 15:37:37 +0200

    public and private routes

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Layout from "./Hoc/Layout";
+import { Switch, Route } from "react-router-dom";
+
+import PrivateRoute from "./Components/authRoutes/privateRoutes";
+import PublicRoute from "./Components/authRoutes/publicRoutes.js";
+
+import Home from "./Components/home";
+import SignIn from "./Components/signin";
+
+import Dashboard from "./Components/admin/Dashboard";
+
+const Routes = props => {
+  return (
+    <Layout>
+      <Switch>
+        {/* TODO:
+            =======================
+            add complete set of admin routes
+        */}
+        <PrivateRoute
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+        <PublicRoute
+          {...props}
+          restricted={true}
+          path="/sign_in"
+          exact
+          component={SignIn}
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/"
+          exact
+          component={Home}
+        />
+      </Switch>
+    </Layout>
+  );
+};
+
+export default Routes;
